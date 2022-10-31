3e2cbdd18bee60eadc5b012e82ac80cd323aed8b
Peeter Org <peeter.org@liewenthal.ee>
2022-10-31 13:38:02 +0200

    move all plugin confs to packer file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,65 +1,6 @@
-require'packer-init'
+require'packer-init' -- load plugins
 
-require'nvim-treesitter.configs'.setup {
-	highlight = {
-		enable = true,
-		custom_captures = {
-			-- Highlight the @foo.bar capture group with the "Identifier" highlight group.
-			-- ["foo.bar"] = "Identifier",
-		},
-	},
-	incremental_selection = {
-		enable = true,
-		keymaps = {
-			init_selection = "gnn",
-			node_incremental = "grn",
-			scope_incremental = "grc",
-			node_decremental = "grm",
-		},
-	},
-	indent = {
-		enable = false
-	},
-	rainbow = {
-		enable = true,
-		extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
-		max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
-	}
-}
-
-local cmp = require'cmp'
-cmp.setup(
-	{
-		sources = {
-			{name = "nvim_lsp"},
-			{name = "buffer"},
-			{name = "nvim_lua"},
-			{name = "path"}
-		},
-		mapping = {
-			['<C-e>'] = cmp.mapping.abort(),
-			["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), {"i", "s"}),
-			["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), {"i", "s"}),
-			["<CR>"] = cmp.mapping.confirm(
-				{
-					behavior = cmp.ConfirmBehavior.Replace,
-					select = true
-				}
-			),
-		}
-	}
-)
-
-
-require'lspconfig'.pyright.setup{}
-require'lspconfig'.clangd.setup{}
-require'lspconfig'.rls.setup{}
-require'nvim-autopairs'.setup()
-require'snippet_conf'
-
-vim.o.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme gruvbox]])
-
+--settings
 vim.g.mapleader = ','
 vim.o.number = true
 vim.o.updatetime = 300
@@ -69,7 +10,7 @@ vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.mouse =
 
-
+--mappings
 vim.api.nvim_set_keymap('n', '<Leader>d', "<cmd>lua require('fzf-lua').tags()<CR>", {noremap = true})
 vim.api.nvim_set_keymap('n', '<Leader>f', "<cmd>lua require('fzf-lua').blines()<CR>", {noremap = true})
 vim.api.nvim_set_keymap('n', '<Leader>g', "<cmd>lua require('fzf-lua').files()<CR>", {noremap = true})
@@ -80,7 +21,6 @@ vim.api.nvim_set_keymap('n', '<Leader>b', ':w<CR> :make -r all<CR>', {noremap = 
 vim.api.nvim_set_keymap('i', '<c-k>', "<cmd>lua return require'snippets'.expand_or_advance(1)<CR>", {noremap = true})
 vim.api.nvim_set_keymap('i', '<c-j>', "<cmd>lua return require'snippets'.advance_snippet(-1)<CR>", {noremap = true})
 vim.api.nvim_set_keymap('n', '<Leader>h', ":DiffviewFileHistory %<CR>", {noremap = true})
-
 
 vim.api.nvim_set_keymap('n', '<Leader>as', '<C-w>l', {noremap = true})
 vim.api.nvim_set_keymap('n', '<Leader>sw', '<C-w>k', {noremap = true})
--- .config/nvim/lua/packer-init.lua
+++ .config/nvim/lua/packer-init.lua
@@ -1,11 +1,67 @@
 return require('packer').startup(function()
-	use 'wbthomason/packer.nvim'
 
-	use 'nvim-treesitter/nvim-treesitter'
+	use 'wbthomason/packer.nvim' --self-manage
 
-	use 'p00f/nvim-ts-rainbow'
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		requires = { 'p00f/nvim-ts-rainbow', opt = true},
+		config = function()
+			require'nvim-treesitter.configs'.setup {
+				highlight = {
+					enable = true,
+					custom_captures = {
+						-- Highlight the @foo.bar capture group with the "Identifier" highlight group.
+						-- ["foo.bar"] = "Identifier",
+					},
+				},
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "gnn",
+						node_incremental = "grn",
+						scope_incremental = "grc",
+						node_decremental = "grm",
+					},
+				},
+				indent = {
+					enable = false --is buggy
+				},
+				rainbow = {
+					enable = true,
+					extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
+					max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
+				}
+			}
+		end
+	}
 
-	use 'hrsh7th/nvim-cmp'
+	use {
+		'hrsh7th/nvim-cmp',
+		config = function()
+			local cmp = require'cmp'
+			cmp.setup(
+			{
+				sources = {
+					{name = "nvim_lsp"},
+					{name = "buffer"},
+					{name = "nvim_lua"},
+					{name = "path"}
+				},
+				mapping = {
+					['<C-e>'] = cmp.mapping.abort(),
+					["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), {"i", "s"}),
+					["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), {"i", "s"}),
+					["<CR>"] = cmp.mapping.confirm(
+					{
+						behavior = cmp.ConfirmBehavior.Replace,
+						select = true
+					}
+					),
+				}
+			}
+			)
+		end
+	}
 
 	use 'hrsh7th/cmp-buffer'
 	
@@ -15,7 +71,15 @@ return require('packer').startup(function()
 
 	use 'hrsh7th/cmp-path'
 
-	use 'neovim/nvim-lspconfig'
+	use {
+		'neovim/nvim-lspconfig',
+		config = function()
+			conf = require'lspconfig'
+			conf.pyright.setup{}
+			conf.clangd.setup{}
+			conf.rls.setup{}
+		end
+	}
 
 	use {
 		'ray-x/lsp_signature.nvim',
@@ -29,7 +93,12 @@ return require('packer').startup(function()
 
 	use 'ibhagwan/fzf-lua'
 
-	use 'windwp/nvim-autopairs'
+	use {
+		'windwp/nvim-autopairs',
+		config = function()
+			require'nvim-autopairs'.setup()
+		end
+	}
 
 	use 'tpope/vim-surround'
 
@@ -59,7 +128,12 @@ return require('packer').startup(function()
 
 	use 'preservim/nerdcommenter'
 
-	use 'norcalli/snippets.nvim'
+	use {
+		'norcalli/snippets.nvim',
+		config = function()
+			require'snippet_conf'
+		end
+	}
 
 	use 'jremmen/vim-ripgrep'
 
@@ -67,7 +141,13 @@ return require('packer').startup(function()
 
 	use 'makerj/vim-pdf'
 
-	use "ellisonleao/gruvbox.nvim"
+	use {
+		"ellisonleao/gruvbox.nvim",
+		config = function()
+			vim.o.background = "dark" -- or "light" for light mode
+			vim.cmd([[colorscheme gruvbox]])
+		end
+	}
 
 	use {
 		"AckslD/nvim-neoclip.lua",
